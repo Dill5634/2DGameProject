4479894cd83d74113adf9a377216b04d37a452a3
Harriet Kirby <hk1114@york.ac.uk>
2024-05-14 23:45:05 +0100

    Added ActivityTypeTest

--- core/test/io/github/uoyeng1g6/constants/ActivityTypeTest.java
+++ core/test/io/github/uoyeng1g6/constants/ActivityTypeTest.java
@@ -1,16 +1,34 @@
 package io.github.uoyeng1g6.constants;
 
+import io.github.uoyeng1g6.GdxTestRunner;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(GdxTestRunner.class)
 class ActivityTypeTest {
 
     @Test
     void values() {
+        assertEquals("[SPOONS, PIAZZA, GLASSHOUSE, BREAKFAST, STUDY, LIBRARY, CSBUILDING, RECREATION, DUCKS, WALK]",
+                Arrays.toString(ActivityType.values()));
     }
 
     @Test
     void valueOf() {
+        assertTrue(ActivityType.EAT.contains(ActivityType.SPOONS));
+        assertTrue(ActivityType.EAT.contains(ActivityType.PIAZZA));
+        assertTrue(ActivityType.EAT.contains(ActivityType.GLASSHOUSE));
+
+        assertTrue(ActivityType.PLAY.contains(ActivityType.RECREATION));
+        assertTrue(ActivityType.PLAY.contains(ActivityType.DUCKS));
+        assertTrue(ActivityType.PLAY.contains(ActivityType.WALK));
+
+        assertTrue(ActivityType.WORK.contains(ActivityType.STUDY));
+        assertTrue(ActivityType.WORK.contains(ActivityType.LIBRARY));
+        assertTrue(ActivityType.WORK.contains(ActivityType.CSBUILDING));
     }
 }
